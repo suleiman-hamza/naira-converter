dd11fc08adffd91822e7e7650290186e1a7c9780
suleiman hamza <hamzasuleiman530@gmail.com>
2024-11-27 09:57:34 +0100

    [Feat]: added country drawer

--- src/views/Homeview.vue
+++ src/views/Homeview.vue
@@ -1,9 +1,11 @@
 <template>
     <Header />
     <Converter />
+    <CountryDrawer />
 </template>
 
 <script setup lang="ts">
 import Header from '@/components/Header.vue';
 import Converter from '@/components/Converter.vue';
+import CountryDrawer from '@/components/CountryDrawer.vue'
 </script>

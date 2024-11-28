b459f183ed9c551f4f6b2a0f0eb70ae038ceaed1
Bewizi <toluwalase.bewaji@yahoo.com>
2024-11-28 01:31:21 +0100

    added click me event to the list item.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="max-w-4xl flex items-center justify-between mb-8">
+  <nav class="max-w-4xl mx-auto flex items-center justify-between mb-8">
     <div>
       <router-link class="font-bold text-2xl" to="">Naira-X</router-link>
     </div>

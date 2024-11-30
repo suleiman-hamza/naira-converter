6293dc634daa5c46551b6adef0514e2b10e9bbc7
suleiman hamza <hamzasuleiman530@gmail.com>
2024-11-30 02:26:41 +0100

    [style]: tailwind utility classes for ui

--- src/components/Converter.vue
+++ src/components/Converter.vue
@@ -32,23 +32,23 @@ watch(localizedAmount, (newVal) => {
 
 </script>
 <template>
-        <Card class="max-w-xs mx-auto mb-8">
+        <Card class="max-w-xs mx-auto mb-8 bg-stone-900 border-gray-600">
             <CardHeader>
-            <CardTitle>Currency Converter</CardTitle>
+            <CardTitle class="text-white">Currency Converter</CardTitle>
             <CardDescription>Convert Nigerian NGN to {{ store.selectedCountry }}</CardDescription>
             </CardHeader>
             <CardContent>
-                <div class="grid items-center w-full gap-4">
+                <div class="grid items-center w-full gap-4 text-white">
                     <div class="flex flex-col space-y-1.5">
-                        <Label for="ngn">NGN</Label>
-                        <Input id="ngn" type="number" v-model="ngnInitialAmount" />
+                        <Label for="ngn" class="text-white">NGN</Label>
+                        <Input id="ngn" type="number" v-model="ngnInitialAmount" class="bg-stone-900 border-gray-500"/>
                     </div>
                     <div class="flex flex-col space-y-1.5">
-                        <Label for="for">{{ store.selectedCountry }}</Label>
-                        <Input id="for" type="number" v-model="localizedAmount"/>
+                        <Label for="for" class="text-white">{{ store.selectedCountry }}</Label>
+                        <Input id="for" type="number" v-model="localizedAmount" class="bg-stone-900 border-gray-500"/>
                     </div>
                     <div class="flex flex-col space-y-1.5">
-                        <Button variant="outline" @click="clearInputs">
+                        <Button variant="outline" @click="clearInputs" class="text-stone-900">
                             Clear
                         </Button>
                     </div>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="max-w-4xl mx-auto flex items-center justify-between mb-8">
+  <nav class="max-w-4xl mx-auto flex items-center justify-between mb-8 border-b border-gray-500 bg-stone-900 px-4 py-4">
     <div>
       <router-link class="font-bold text-2xl" to="">Naira-X</router-link>
     </div>
